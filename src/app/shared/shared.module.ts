09a88ed00487238a698977247d4539dde69f129f
Pablo A <pabloalvarezseguel@gmail.com>
2021-04-16 09:54:47 -0400

    seccion 8 clase 101

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
+
+
 import { SidebarComponent } from './sidebar/sidebar.component';
 
 
@@ -9,7 +12,8 @@ import { SidebarComponent } from './sidebar/sidebar.component';
     SidebarComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule,
   ],
   exports :[
     SidebarComponent,
